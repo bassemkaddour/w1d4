1b792684be4ab9d63a5339248be26a8046864281
Bassem Kaddour <bassemkaddour@gmail.com>
2018-09-27 19:25:55 +0000

    age sorting exercise

---
+++ sort-names.js
@@ -0,0 +1,20 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+function sortStudents(studentArray) {
+  studentArray.sort(function(a, b) {
+    if (a.name[0] > b.name[0]) {
+      return 1;
+    } else if (a.name[0] === b.name[0] && a.age < b.age) {
+      return 1;
+    }
+
+  });
+  console.log(studentArray);
+}
+
+sortStudents(students);
